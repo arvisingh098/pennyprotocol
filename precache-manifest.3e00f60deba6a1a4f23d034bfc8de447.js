5b61c9dd6fa491ce13e770adb6b7385bd86e5175
arvisingh098 <singharvi090@gmail.com>
2021-01-14 01:05:33 +0530

    Updates

---
+++ precache-manifest.3e00f60deba6a1a4f23d034bfc8de447.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b627648ccb39390175acdadb5eb1cb",
+    "url": "/pennyprotocol/index.html"
+  },
+  {
+    "revision": "6fe5745eb883df7c985f",
+    "url": "/pennyprotocol/static/css/3.90857bd9.chunk.css"
+  },
+  {
+    "revision": "e0e42afdb04722b17505",
+    "url": "/pennyprotocol/static/css/main.cd51d495.chunk.css"
+  },
+  {
+    "revision": "d1dc5d63e7978762f14b",
+    "url": "/pennyprotocol/static/js/0.190c78fc.chunk.js"
+  },
+  {
+    "revision": "a6847b131eaf85fdd18f",
+    "url": "/pennyprotocol/static/js/10.0e249399.chunk.js"
+  },
+  {
+    "revision": "6fe5745eb883df7c985f",
+    "url": "/pennyprotocol/static/js/3.0d84452a.chunk.js"
+  },
+  {
+    "revision": "4d227fb27fd06e1f21c21b13e105891f",
+    "url": "/pennyprotocol/static/js/3.0d84452a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0d2af88fed920ff0b9",
+    "url": "/pennyprotocol/static/js/4.fbe7beea.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/pennyprotocol/static/js/4.fbe7beea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502ef00b35e0c70d18bf",
+    "url": "/pennyprotocol/static/js/5.3b141c2f.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/pennyprotocol/static/js/5.3b141c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d679999434c1f333e8",
+    "url": "/pennyprotocol/static/js/6.911cfea5.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/pennyprotocol/static/js/6.911cfea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e5502963e88bdfb2a6",
+    "url": "/pennyprotocol/static/js/7.7f3e7a4b.chunk.js"
+  },
+  {
+    "revision": "da7f2c7a95435536657f",
+    "url": "/pennyprotocol/static/js/8.797d038c.chunk.js"
+  },
+  {
+    "revision": "8a4394fe141506e34f1f",
+    "url": "/pennyprotocol/static/js/9.64f3b83d.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/pennyprotocol/static/js/9.64f3b83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e42afdb04722b17505",
+    "url": "/pennyprotocol/static/js/main.45c4f3d7.chunk.js"
+  },
+  {
+    "revision": "2f217b3bd3b5c8ecbd80",
+    "url": "/pennyprotocol/static/js/runtime-main.484d42a6.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/pennyprotocol/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/pennyprotocol/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/pennyprotocol/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/pennyprotocol/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/pennyprotocol/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
